07d9a679e2e2d2a23ad6b326939c988176f24eb3
Mark Neumann <mark.neumann.1992@gmail.com>
2025-03-18 17:13:24 -0700

    add spherical harmonics

--- orb_models/forcefield/rbf.py
+++ orb_models/forcefield/rbf.py
@@ -1,6 +1,7 @@
 import math
 
 import torch
+import numpy as np
 
 
 class CosineCutoff(torch.nn.Module):
@@ -58,6 +59,7 @@ class ExpNormalSmearing(torch.nn.Module):
         self.cutoff_lower = cutoff_lower
         self.cutoff_upper = cutoff_upper
         self.num_rbf = num_rbf
+        self.num_bases = num_rbf  # Compatability
         self.trainable = trainable
 
         self.cutoff_fn = CosineCutoff(0, cutoff_upper)
@@ -96,4 +98,49 @@ class ExpNormalSmearing(torch.nn.Module):
         return self.cutoff_fn(dist) * torch.exp(
             -self.betas
             * (torch.exp(self.alpha * (-dist + self.cutoff_lower)) - self.means) ** 2  # type: ignore
+        )
+
+
+class BesselBasis(torch.nn.Module):
+    """Bessel basis functions.
+    NOTE: whilst similar to SphericalBesselBasis, this class
+    has different bessel_weights and prefactor.
+    """
+
+    def __init__(self, r_max: float, num_bases=8, trainable=False):
+        super().__init__()
+        self.num_bases = num_bases
+        bessel_weights = (
+            np.pi
+            / r_max
+            * torch.linspace(
+                start=1.0,
+                end=num_bases,
+                steps=num_bases,
+                dtype=torch.get_default_dtype(),
+            )
+        )
+        if trainable:
+            self.bessel_weights = torch.nn.Parameter(bessel_weights)
+        else:
+            self.register_buffer("bessel_weights", bessel_weights)
+
+        self.register_buffer(
+            "r_max", torch.tensor(r_max, dtype=torch.get_default_dtype())
+        )
+        self.register_buffer(
+            "prefactor",
+            torch.tensor(np.sqrt(2.0 / r_max), dtype=torch.get_default_dtype()),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:  # [..., 1]
+        """Compute the basis function."""
+        numerator = torch.sin(self.bessel_weights * x[:, None])  # [..., num_basis]
+        return self.prefactor * (numerator / x[:, None])  # type: ignore
+
+    def __repr__(self):
+        """Return a string representation of the basis function."""
+        return (
+            f"{self.__class__.__name__}(r_max={self.r_max}, num_basis={len(self.bessel_weights)}, "
+            f"trainable={self.bessel_weights.requires_grad})"
         )
